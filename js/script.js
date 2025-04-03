8fbea14f63421cf70895475d90037325bbaf2c75
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-03 16:37:48 +0200

    [UPD] E-MAIL DA MOSTRARE A VIDEO

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-axios.get(' https://flynn.boolean.careers/exercises/api/random/mail').then((element)=>{
-  console.log(element);  
+const mostrareAVideo = document.getElementById('emailDaGenerare');
+let numero;
+
+
+axios.get(' https://flynn.boolean.careers/exercises/api/random/mail').then((elemento)=>{
+  numero = elemento.data.response;
+  mostrareAVideo.innerHTML = numero;
+  
 })
