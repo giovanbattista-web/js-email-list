055240dea391ada79e099e54d8872c2148314ffc
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-03 17:26:59 +0200

    [UPD] PRENDO GLI ELEMENTI DAL DOM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-const mostrareAVideo = document.getElementById('emailDaGenerare');
-let numero;
+const primaEmail = document.getElementById('mail1');
+const secondaEmail = document.getElementById('mail2');
+const terzaEmail = document.getElementById('mail3');
+const quartaEmail = document.getElementById('mail4');
+const quintaEmail = document.getElementById('mail5');
+const sestaEmail = document.getElementById('mail6');
+const settimaEmail = document.getElementById('mail7');
+const ottavaEmail = document.getElementById('mail8');
+const nonaEmail = document.getElementById('mail9');
+const decimaEmail = document.getElementById('mail10');
+
 
 
 axios.get(' https://flynn.boolean.careers/exercises/api/random/mail').then((elemento)=>{
