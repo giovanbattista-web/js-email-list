e87439aa4b5c89ceee7f1c2eecdc5df0fc65b0ac
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-03 07:05:45 +0200

    [UPD] ULTERIORE MODIFICA AL FOR FATTA DAL PROFESSORE

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ const list = document.getElementById('emails-list');
 // DICHIARAZIONE DELLE VARIABILI CHE CI SERVONO
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-for (let i=0; i<10; i++){
+for (let i=1; i<=10; i++){
     axios.get(endpoint).then((res) => {
       let li = `<li>${res.data.response}</li>`;
       list.innerHTML += li;
